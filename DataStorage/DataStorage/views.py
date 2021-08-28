b13d09291b0857e4fc81bca180ca6a518b56295e
rhming <49028484+rhming@users.noreply.github.com>
2021-08-28 10:29:44 +0800

    数据存储服务

--- DataStorage/DataStorage/views.py
+++ DataStorage/DataStorage/views.py
@@ -31,25 +31,25 @@ class DataSerializer(serializers.ModelSerializer):
 
 class DataManagerView(viewsets.ModelViewSet):
     serializer_class = DataSerializer
-    valid = None
+    authentication_classes = []
+    permission_classes = []
+    valid = False
     result = None
     extra = None
-
     def list(self, request: HttpRequest, *args, **kwargs):
         try:
-            data = request.GET.dict()
+            data = request.GET
             data_serializer = self.get_serializer(data=data)
             self.valid = data_serializer.is_valid(raise_exception=True)
             self.result = data_serializer.get()
+            self.valid = bool(self.result and self.valid)
         except Exception as e:
-            self.valid = False
             self.extra = e.args
         return Response({
             'msg': self.valid,
             'data': self.result,
             'extra': self.extra
         })
-
     def create(self, request: HttpRequest, *args, **kwargs):
         try:
             data = request.POST or request.data
@@ -57,7 +57,6 @@ class DataManagerView(viewsets.ModelViewSet):
             self.valid = data_serializer.is_valid(raise_exception=True)
             self.result = data_serializer.update_or_create()
         except Exception as e:
-            self.valid = False
             self.extra = e.args
         return Response({
             'msg': self.valid,
